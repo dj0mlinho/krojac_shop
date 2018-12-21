84dc35d59678f2463d61dcb9ee86d226b40f0d9d
dj0mlinho <30881187+dj0mlinho@users.noreply.github.com>
2018-12-21 08:36:45 +0100

    2. commit - finished first page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,97 @@
+let db;
+let template = $('[type = "template"]').html();
+
+
+$.ajax({
+    url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
+    dataType: "json"
+  })
+
+  .done(startApp)
+
+function startApp(res) {
+  db = res;
+
+
+  render(db)
+
+  $("[data-col]").on('click', displayCategory)
+
+}
+
+function displayCategory() {
+  event.preventDefault()
+  let dataCol = $(this).data('col')
+
+  let colection = db.filter(function (element) {
+    return element.colection == dataCol || element[dataCol];
+  })
+  render(colection)
+
+}
+
+
+function render(colection) {
+  let text = "";
+  for (let i = 0; i < colection.length; i++) {
+    text += template.replace(/{{imgSrc}}/gi, colection[i].imgSrc)
+      .replace(/{{productTitle}}/gi, colection[i].productTitle)
+      .replace(/{{model}}/gi, colection[i].model)
+      .replace(/{{price}}/gi, colection[i].price)
+  }
+  $('#insertTemplate').html(text);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//BOJANOVO
+
 $(".back-to-top").click(function () {
-  $("html, body").animate({scrollTop: 0}, 1000);
+  $("html, body").animate({
+    scrollTop: 0
+  }, 1000);
 });
