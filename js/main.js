160568d3de6f55003ae473db129e4b3da5e6c087
dj0mlinho <30881187+dj0mlinho@users.noreply.github.com>
2018-12-22 06:55:10 +0100

    3. commit - link to second page

--- js/main.js
+++ js/main.js
@@ -1,97 +1,56 @@
 let db;
 let template = $('[type = "template"]').html();
 
-
 $.ajax({
-    url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
-    dataType: "json"
-  })
-
-  .done(startApp)
+  url: "https://raw.githubusercontent.com/Danilovesovic/shop/master/shop.json",
+  dataType: "json"
+}).done(startApp);
 
 function startApp(res) {
   db = res;
+  render(db);
 
-
-  render(db)
-
-  $("[data-col]").on('click', displayCategory)
-
+  $("[data-col]").on("click", displayCategory);
+  $(".product-img-holder > a").on("click", showProduct);
 }
 
 function displayCategory() {
-  event.preventDefault()
-  let dataCol = $(this).data('col')
+  event.preventDefault();
+  let dataCol = $(this).data("col");
 
-  let colection = db.filter(function (element) {
+  let colection = db.filter(function(element) {
     return element.colection == dataCol || element[dataCol];
-  })
-  render(colection)
-
+  });
+  render(colection);
 }
-
 
 function render(colection) {
   let text = "";
   for (let i = 0; i < colection.length; i++) {
-    text += template.replace(/{{imgSrc}}/gi, colection[i].imgSrc)
+    text += template
+      .replace(/{{imgSrc}}/gi, colection[i].imgSrc)
       .replace(/{{productTitle}}/gi, colection[i].productTitle)
       .replace(/{{model}}/gi, colection[i].model)
-      .replace(/{{price}}/gi, colection[i].price)
+      .replace(/{{price}}/gi, colection[i].price);
   }
-  $('#insertTemplate').html(text);
+  $("#insertTemplate").html(text);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function showProduct() {
+  let imgValue = $(this)
+    .children()
+    .attr("src");
+  this.href = "http://localhost/_projects/shop/detailed.html";
+  localStorage.setItem("save", imgValue);
+}
 
 //BOJANOVO
 
-$(".back-to-top").click(function () {
-  $("html, body").animate({
-    scrollTop: 0
-  }, 1000);
+$(".back-to-top").click(function() {
+  $("html, body").animate(
+    {
+      scrollTop: 0
+    },
+    1000
+  );
 });
